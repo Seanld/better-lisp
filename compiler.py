a4a4efb740eae0ccd68a0dbaac3872fab90eb595
Sean Wilkerson <seanldmcdonald@gmail.com>
2018-10-19 03:04:01 -0600

    Compiles AST into valid VM code

--- compiler.py
+++ compiler.py
@@ -3,3 +3,51 @@
 from tokenizer import *
 from syntaxtree import *
 
+def walk(level):
+    current_stack = []
+
+    try:
+        for node in level[1:]:
+            if type(node) == list:
+                current_stack += walk(node)
+            else:
+                current_stack.append(node)
+
+    except TypeError:
+        current_stack.append(level)
+    
+    current_stack.append(level[0])
+    
+    return current_stack
+
+RENAMES = {
+    "PLUS": "+",
+    "MINUS": "-",
+    "TIMES": "*",
+    "DIVIDE": "/"
+}
+
+def rename(token):
+    if token.type_name == "ATOM":
+        return token.value.group()
+
+    elif token.type_name == "INTEGER":
+        return "#" + token.value.group()
+    
+    elif token.type_name in RENAMES:
+        return RENAMES[token.type_name]
+    
+    else:
+        return token.type_name
+
+def compile_ast(tree):
+    stack = walk(tree)
+
+    counter = 0
+
+    for token in stack:
+        stack = stack[:counter] + [rename(token)] + stack[counter + 1:]
+
+        counter += 1
+    
+    return stack
